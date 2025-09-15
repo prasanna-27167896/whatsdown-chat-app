429e82514b2d50b58b33079a2df4c4a65aa6411d
prasanna kumar <vprasannakumar2716@gmail.com>
2025-09-15 13:15:24 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://whatsdown-chat-app.vercel.app",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://whatsdown-chat-app.vercel.app",
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
